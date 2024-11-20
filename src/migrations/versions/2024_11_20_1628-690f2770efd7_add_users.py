13d1426c2c9051f0e648b109d85d9b30764bff11
shoneaura <oreol1313@yandex.ru>
2024-11-20 16:30:18 +0300

    Задание №8: Запретить создание нескольких юзеров с одинаковой почтой

---
+++ src/migrations/versions/2024_11_20_1628-690f2770efd7_add_users.py
@@ -0,0 +1,27 @@
+"""add users
+
+Revision ID: 690f2770efd7
+Revises: b1c0041b296e
+Create Date: 2024-11-20 16:28:22.086350
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "690f2770efd7"
+down_revision: Union[str, None] = "b1c0041b296e"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_unique_constraint(None, "users", ["email"])
+
+
+def downgrade() -> None:
+    op.drop_constraint(None, "users", type_="unique")
