4ddc96b2c5e94a03066371024167ce3b8f43eed8
shoneaura <oreol1313@yandex.ru>
2025-07-11 14:43:28 +0300

    Задание №15: Получение и добавление удобств

---
+++ src/api/facilities.py
@@ -0,0 +1,29 @@
+from datetime import date
+
+from fastapi import APIRouter, Body
+
+from src.api.dependencies import DBDep
+from src.schemas.facilities import FacilitiesAdd
+
+router = APIRouter(prefix="/facilities", tags=["Удобства"])
+
+
+@router.get(
+    "", summary="Получение списка удобств", description="Возвращает весь список удобств"
+)
+async def get_facilities(db: DBDep):
+    return await db.facilities.get_all()
+
+
+@router.post(
+    "",
+    summary="Добавление удобства",
+    description="Добавляет удобство",
+)
+async def create_facilities(
+    db: DBDep,
+    facilities_data: FacilitiesAdd = Body(),
+):
+    facility = await db.facilities.add(facilities_data)
+    await db.commit()
+    return {"status": "OK", "data": facility}
