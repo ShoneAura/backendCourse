3f8f67a0161f196cb53707ccd4622d2137a827ee
shoneaura <oreol1313@yandex.ru>
2025-07-14 15:24:46 +0300

    Задание №19: Вставить в тестовую базу тестовые данные

---
+++ tests/conftest.py
@@ -0,0 +1,56 @@
+import json
+
+import pytest
+
+from src.config import settings
+from src.database import engine_null_pool, async_session_maker_null_pool
+from src.main import app
+from src.models import *
+from httpx import AsyncClient
+
+from src.models.base import Base
+from src.schemas.hotels import HotelAdd
+from src.schemas.rooms import RoomAdd
+from src.utils.db_manager import DBManager
+
+
+@pytest.fixture(scope="session", autouse=True)
+def check_test_mode():
+    assert settings.MODE == "TEST"
+
+
+@pytest.fixture(scope="function")
+async def db() -> DBManager:
+    async with DBManager(session_factory=async_session_maker_null_pool) as db:
+        yield db
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def setup_database(check_test_mode):
+    async with engine_null_pool.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+    with open("tests/mock_hotels.json", encoding="utf-8") as file_hotels:
+        hotels = json.load(file_hotels)
+    with open("tests/mock_rooms.json", encoding="utf-8") as file_rooms:
+        rooms = json.load(file_rooms)
+
+    hotels = [HotelAdd.model_validate(hotel) for hotel in hotels]
+    rooms = [RoomAdd.model_validate(room) for room in rooms]
+
+    async with DBManager(session_factory=async_session_maker_null_pool) as db_:
+        await db_.hotels.add_bulk(hotels)
+        await db_.rooms.add_bulk(rooms)
+        await db_.commit()
+
+
+@pytest.fixture(scope="session")
+async def ac() -> AsyncClient:
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        yield ac
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def register_user(ac, setup_database):
+    await ac.post("/auth/register", json={"email": "kot@pes.com", "password": "1234"})
